fb5bafff56131f7d6bb21a82cc0c74c1365293c3
raina <raina@rainadeMacBook-Pro.local>
2025-11-10 16:47:57 +1100

    Modify main block; tqdm Added

--- Model/machine-learning/method1.py
+++ Model/machine-learning/method1.py
@@ -6,6 +6,8 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
+from datetime import datetime
 from sift import extract_sift_features
 from hog import extract_hog_features
 from svm import train_svm, evaluate_svm
@@ -34,17 +36,17 @@ def evaluate_model(name, real_label, predicted_label):
 
     cm_display = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels=np.unique(real_label))
     cm_display.plot()
-    # plt.show()
+    plt.show()
 
 
 def extract_features(processed_img): 
     X = [] 
     Y = [] 
-    for (id, img) in processed_img: 
-        sift_descriptors = extract_sift_features(img, 150) 
+    for (id, img) in tqdm(processed_img, desc="Extracting features", unit="img"): 
+        sift_descriptors, kps = extract_sift_features(img, 150)
 
         hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV) 
-        mask = cv2.inRange(hsv, (0, 30, 50), (180, 255, 255)) 
+        mask = cv2.inRange(hsv, (0, 70, 50), (180, 255, 255)) 
         # keep only mid-bright objects
         masked = cv2.bitwise_and(img, img, mask=mask) 
         hog_descriptors = extract_hog_features(masked) 
@@ -52,30 +54,53 @@ def extract_features(processed_img):
         descriptors = np.hstack((sift_descriptors, hog_descriptors)) 
         X.append(descriptors) 
         Y.append(id) 
-        
+
     return np.array(X), np.array(Y)
 
 if __name__ == "__main__":
-    image_path = "../../Data/test/images"
-    label_path = "../../Data/test/labels"
+    image_path = "../../Data/train/images"
+    label_path = "../../Data/train/labels"
 
+    print("Reading and preprocessing training images...")
     processed_img = read_images(image_path, label_path, apply_preprocess=True, output_rgb=True)
 
-    X, Y = extract_features(processed_img)
+    labels = np.array([cls for cls, _ in processed_img])
 
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=42)
+    # Use 50% of dataset for convenience
+    _, sampled_img, _, _ = train_test_split(
+        processed_img, labels,
+        test_size=0.5,
+        stratify=labels,
+        random_state=42
+    )
 
-    clf = train_svm(X_train, y_train, tune=False)
+    print("Extracting features from training set...")
+    X, Y = extract_features(sampled_img)
+
+    print(f"{len(X)}Freature Extracted")
+
+    # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=42)
+
+    clf = train_svm(X, Y, tune=False)
+
+    print("SVM TRAINED")
+
+    test_image_path = "../../Data/test/images"
+    test_label_path = "../../Data/test/labels"
+
+    processed_test_img = read_images(test_image_path, test_label_path, apply_preprocess=True, output_rgb=True)
+
+    X_test, Y_test = extract_features(processed_test_img)
 
     pre_label = evaluate_svm(clf, X_test)
+
+    # print(np.unique(y_train, return_counts=True))
     
     print(np.unique(pre_label, return_counts=True))
-    print(np.unique(y_test, return_counts=True))
-
-    now = datetime.now()
+    print(np.unique(Y_test, return_counts=True))
 
     with open('report', 'a+') as file:
-        file.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')} {accuracy_score(pre_label, y_test):.4f}\n")
+        now = datetime.now()
+        file.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')} {accuracy_score(pre_label, Y_test):.4f}\n")
 
-    evaluate_model("Method 1", y_test, pre_label)
-
+    evaluate_model("Method 1", Y_test, pre_label)
